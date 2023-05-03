b9282ceec417d0dab96308a0c6539dc7ab25b660
Nguyễn Trung Nguyên <nnguyentrung8324@gmail.com>
2023-05-03 18:20:58 +0700

    fix: sua ui

--- AdminFrontEnd/admin/src/pages/Login.js
+++ AdminFrontEnd/admin/src/pages/Login.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react'
 import { Link, Redirect } from 'react-router-dom';
 import './stylePage/login.css';
-import {connect} from 'react-redux';
-import {fetch_auth} from '../actions/authAction';
-import {Spin} from 'antd'
+import { connect } from 'react-redux';
+import { fetch_auth } from '../actions/authAction';
+import { Spin } from 'antd'
 
 class Login extends Component {
-    
-    handleSubmit(e){
+
+    handleSubmit(e) {
         e.preventDefault();
         //console.log(e.target.username.value);
-        
-        this.props.login({username: e.target.username.value, password: e.target.password.value});
+
+        this.props.login({ username: e.target.username.value, password: e.target.password.value });
     }
     haveRedirect() {
         const { isAuthenticated } = this.props;
         if (isAuthenticated === true) {
-           
-           return <Redirect to="/admin"></Redirect>
+
+            return <Redirect to="/admin"></Redirect>
         }
     }
-    
+
     render() {
-        
-        const {isLoading} = this.props; 
-        if(isLoading)
+
+        const { isLoading } = this.props;
+        if (isLoading)
             return (<Spin size="large" tip="LOADING"></Spin>)
         return (
             <div className="wrapper">
@@ -35,10 +35,10 @@ class Login extends Component {
                 <form onSubmit={this.handleSubmit.bind(this)}>
                     <input type="text" placeholder="ID" name="username"></input>
                     <input type="password" placeholder="Mật khẩu" name="password"></input>
-                    <button type="submit">ENTER</button>
+                    <button type="submit">Sign in</button>
                 </form>
                 <div className="bottom-text">
-                    
+
                 </div>
             </div>
         )
@@ -52,7 +52,7 @@ const mapStateToProps = (state) => {
 }
 const mapDispatchToProps = (dispatch) => {
     return {
-        login: (data) => {dispatch(fetch_auth(data))},
+        login: (data) => { dispatch(fetch_auth(data)) },
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- AdminFrontEnd/admin/src/pages/Manage/orderManage/MainScreen.js
+++ AdminFrontEnd/admin/src/pages/Manage/orderManage/MainScreen.js
@@ -1,82 +1,43 @@
 import React, { Component } from 'react';
-import {Row, Col, Statistic, Button, Card} from 'antd';
+import { Row, Col, Statistic, Button, Card } from 'antd';
 import Header from '../../../components/common/Header';
 import Sidebar from '../../../components/common/Sidebar';
 import '../../../components/common/styleCommon/Content.css';
 import BreadScrumb from '../../../components/breadScrumb/BreadScrumb';
-import {EditOutlined, LikeOutlined, ArrowUpOutlined, ArrowDownOutlined, SyncOutlined} from '@ant-design/icons';
+import { EditOutlined, LikeOutlined, ArrowUpOutlined, ArrowDownOutlined, SyncOutlined } from '@ant-design/icons';
 import moment from 'moment';
 import axiosInstance from '../../../utils/axiosInstance';
 import queryString from 'querystring';
+import { connect } from 'react-redux';
 
-export default class MainScreen extends Component {
+class MainScreen extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
             <>
-            <Header></Header>   
+                <Header></Header>
                 <div className="main_container">
                     <Sidebar isActive="13"></Sidebar>
                     <div className="content">
                         <BreadScrumb title="Home"></BreadScrumb>
                         <br></br>
-                        
-                        <Row>
-                            <Col span={22} offset={1}>
-                            <div className="site-statistic-demo-card">
-                            <Row gutter={16}>
-                            <Col span={12}>
-                                <Card>
-                                <Statistic
-                                    title="Active"
-                                    value={11.28}
-                                    precision={2}
-                                    valueStyle={{ color: '#3f8600' }}
-                                    prefix={<ArrowUpOutlined />}
-                                    suffix="%"
-                                />
-                                </Card>
-                            </Col>
-                            <Col span={12}>
-                                <Card>
-                                <Statistic
-                                    title="Idle"
-                                    value={9.3}
-                                    precision={2}
-                                    valueStyle={{ color: '#cf1322' }}
-                                    prefix={<ArrowDownOutlined />}
-                                    suffix="%"
-                                />
-                                </Card>
-                            </Col>
-                            </Row>
+                        <div style={{ marginLeft: 24 }}>
+                            Hello {this.props.name}
                         </div>
-                        <br></br>
-                            <Row >
-                            <Col span={12}>
-                            <Statistic title="Active Users" value={112893} />
-                            </Col>
-                            <Col span={12}>
-                            <Statistic title="Account Balance (CNY)" value={112893} precision={2} />
-                            <Button style={{ marginTop: 16 }} type="primary">
-                                Recharge
-                            </Button>
-                            </Col>
-                        </Row>
-                        <br></br>
-                        <Row >
-                            <Col span={12}>
-                            <Statistic title="Feedback" value={1128} prefix={<LikeOutlined />} />
-                            </Col>
-                            <Col span={12}>
-                            <Statistic title="Unmerged" value={93} suffix="/ 100" />
-                            </Col>
-                        </Row>
-                        </Col>
-                        </Row>
                     </div>
                 </div>
             </>
-            
+
         )
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        name: state.auth.nameUser,
+        avatar: state.auth.avatar
+    }
+}
+
+export default connect(mapStateToProps)(MainScreen);
